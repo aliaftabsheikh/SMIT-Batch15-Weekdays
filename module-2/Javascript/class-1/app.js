7efdfa5d392d9cb32d094e344925558e00650f6b
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-01-24 17:22:11 +0500

    js-class-1

---
+++ module-2/Javascript/class-1/app.js
@@ -0,0 +1,55 @@
+// console.log("Hello World !!!")
+
+
+// alert("Hello World !!!")
+
+
+// GLOBAL VARIABLE
+
+// VAR 
+
+// var a = 20;
+
+// console.log(a)
+
+
+// var a = 30
+
+// console.log(a)
+
+
+
+
+
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+
+
+// LET & CONST
+
+// LOCAL VARIABLE
+
+// let b = 20;
+
+// b = 40;
+
+
+// console.log(b)
+
+
+// const c = 30;
+// c = 40;
+
+// console.log(c)
+
+
+
+// var a = 20;
+
+// const a = 40;
+// console.log(a)
