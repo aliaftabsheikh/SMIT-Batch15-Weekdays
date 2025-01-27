9ebba2a17f2f957531cd35394df1e68232510711
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-01-27 15:59:46 +0500

    class-3

---
+++ module-2/Javascript/class-2/app.js
@@ -0,0 +1,90 @@
+// Hoisting
+
+// console.log("LINE 1", a);
+
+// let a = "Ali";
+
+// console.log("Line 5", a);
+
+
+
+
+// let firstName = "Ali";
+
+// let lastName = "Aftab";
+
+// let fullName = firstName + ' ' + lastName
+
+// console.log(fullName)
+
+
+// const firstName = "Ali";
+
+// const id = 2
+
+// let fullName = firstName +  id
+
+// console.log(fullName)
+
+// const num1 = "10"
+
+// const num2 = 2
+
+// const sum = num1 - num2
+
+// console.log(sum)
+
+// console.log("10" == 10)
+
+// console.log(typeof(10.3 ))
+
+
+// let firstName = "Ali";
+
+
+// DATA TYPES IN JAVASCRIPT
+
+// const fname = "Ali"; // STRING
+// console.log(fname)
+
+
+// const age = 20; // NUMBER
+// console.log(age)
+
+// const isTrue = true; // BOOLEAN
+// const isFalse = false; // 
+
+// const checkNull = null; // NULL
+
+// const checkUndefined = undefined; // UNDEFINED
+
+// const object = {} //OBJECT
+
+// const arr = [] // ARRAY
+
+const fname = "Anus";
+
+// alert("Welcome " + fname + " to our website")
+
+// alert(`Welcome ${fname} to our Website`)
+
+
+let age = prompt('How old are you?', 20)
+
+
+if (age >= 18) {
+    console.log("You are eligible to vote")
+} else {
+    console.log("Agli bar ana !!!")
+}
+
+
+// console.log(age)
+
+// console.log(`You are ${age} years old!`);
+
+
+
+// CHAPTER 1
+
+// alert("Welcome to our website")
