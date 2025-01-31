6762890b8d97faa2eb669aa71bc96291b016c376
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-01-31 16:28:54 +0500

    class-4

---
+++ module-2/Javascript/class-3/app.js
@@ -0,0 +1,88 @@
+// alert("Hello World !!")
+
+// const age = Number(prompt("Enter your age", 100)) ;
+// const age = "18" ;
+
+// console.log(age === 18)
+
+
+// let isBoss = confirm("Are you the boss?");
+
+// if(isBoss) {
+//     console.log("Hello Boss")
+// } else {
+//     console.log("Hello Guest")
+// }
+
+
+// let value = "21"
+
+// if(typeof value === "string"){
+//     console.log("Succesfully convert to Number", Number(value))
+// }
+
+
+// console.log("10" / "20")
+
+
+// BODMAS RULE (Brackets, Orders, Division and Multiplication, Addition and Subtraction)
+
+// const calculate = 2 + 6 / 2
+
+// console.log(calculate)
+
+
+//Give some example regarding BODMAS rule 
+
+// 1. 2 + 6 / 2
+// 2. 2 + 6 / 2 * 2
+// 3. 2 + 6 / 2 * 2 - 1
+// 4. 2 + 6 / 2 * 2 - 1 + 2
+// 5. 2 + 6 / 2 * 2 - 1 + 2 * 2
+// 6. 2 + 6 / 2 * 2 - 1 + 2 * 2 / 2
+// 7. 2 + 6 / 2 * 2 - 1 + 2 * 2 / 2 + 1
+// 8. 2 + 6 / 2 * 2 - 1 + 2 * 2 / 2 + 1 - 1
+
+
+// const calculate =  (4 + 9) / 2 * (2 - 7) + 5
+
+                //    13 / 2 * -5 + 5
+                //    6.5 * -5 + 5
+                //    -32.5 + 5
+                //    -27.5
+
+// const calculate =   5 + 6 / (2 * 2) - 9 + 2 * 7 / (9 / 3) * 6
+
+//                  5 + 6 / 4 - 9 + 2 * 7 / 3 * 6
+//                  5 + 1.5 - 9 + 2 * 2.3 * 6
+//                  5 + 1.5 - 9 + 27.6
+//                  6.5 - 9 + 27.6
+//                  -2.5 + 27.6
+//                  25.1
+
+
+
+
+
+// console.log(calculate)
+
+
+// console.log(2 % 4 )
+
+
+
+// const exponentiation = 7 ** 3
+//                      7 * 7 * 7 
+// console.log(4 ** 0.5)
+
+
+// let firstName = "Ali";
+// let lastName = "Aftab";
+
+// let fullName = `${firstName} ${lastName}`
+
+// console.log(fullName)
+
+
+const concatination = 2 + 2 + "5" + 2 + 2
+console.log(concatination)
---
+++ module-2/Javascript/class-4/app.js
@@ -0,0 +1,45 @@
+// PRE INCREMENT --- POST INCREMENT 
+
+// let x = 5
+// x++;  //6
+// ++x;  //7
+
+// console.log(x++)  //7 ----> 8
+// console.log(++x)   //9 ----> 9
+
+//POST DECREMENT --- PRE DECREMENT
+
+// let x = 5
+// x--;  //4
+
+// console.log(--x)  //3 ----> 3
+// console.log(x--)  
+
+
+
+// Create most complex examples about pre and post increment and decrement operators without answers.
+
+// let x = 5
+let y = 5
+        
+// let z = x++;
+// let z1 = x++;
+
+// console.log(x++)
+
+// console.log(z + z1)  
+
+
+
+// console.log(x)
+
+   //       6 + 6 + 6 + 6 + 7
+console.log(++y + y++ + --y + y++ + y)
+
+// let a = 5
+// let b = 5
+
+
+
+// console.log(a-- + a-- + a-- + a-- + a-- + a-- + a-- + a-- + a-- + a--)
+// console.log(--b + --b + --b + --b + --b + --b + --b + --b + --b + --b)
