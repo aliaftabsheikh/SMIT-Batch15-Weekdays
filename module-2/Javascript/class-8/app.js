f830e6a302f07bcf1e5d7b7e6ceb4331e493c391
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-02-10 16:12:28 +0500

    class-8

---
+++ module-2/Javascript/class-8/app.js
@@ -0,0 +1,85 @@
+// let name = "Ali";
+// let age = 134;
+// let isStudent = false
+
+
+         
+
+    //         0     1       2   3          4        5         6
+// let person = [ 23, false, "Ali","Arham", "Waqar", "Zeeshan", "anus"]
+// person[1] = true
+
+
+//  6 
+// person.length - 1
+
+// console.log(person[person.length - 1])
+
+
+
+
+// let person1 = {
+//     name : "Ali",
+//     age: 23,
+//     isStudent: false
+// }
+
+
+// console.log(person1.name)
+
+
+
+
+const fruits = ["Mango", "Apple", "Banana", "Strawberry", "Grapes", "Pinapple", "Orange"]
+
+// console.log(fruits.length)
+
+//! ADD Value in last !
+
+// fruits.push("Chiko", "Kiwi")
+
+//! Remove value from last!
+
+// fruits.pop()
+
+
+//! Remove first value !
+
+// fruits.shift()
+// fruits.shift()
+
+
+//! ADD first value
+
+// fruits.unshift("Water Melon")
+
+
+// console.log(fruits)
+
+
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+// console.log(fruits[4])
+// console.log(fruits[5])
+// console.log(fruits[6])
+
+
+for(let i = 0; i < fruits.length; i++){
+    // console.log("Index " + i , " : " + fruits[i])
+
+    if(fruits[i]  === "Orange"){
+        console.log("Orange Milgaaya " + "Gali no "+ i + " se lejana")
+    }
+
+    // TEMPLATE LITERALS
+
+    // console.log(`Index ${i} : ${fruits[i]}`)
+}
+
+//! SHABANA EXAMPLE
+
+// for(let i = 1; i <= 150; i++){
+//     console.log("SORRY Shabana  !!")
+// }
