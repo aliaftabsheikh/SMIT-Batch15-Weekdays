464f1ae6ac0d0752fc5c8095d651d8a7b8ef4dbc
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-02-19 15:49:25 +0500

    class-12

---
+++ module-2/Javascript/class-11/app.js
@@ -0,0 +1,134 @@
+//  FOR LOOP 
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// const fruits = ["Mango", "Apple", "Banana", "Grapes", "Kiwi"]
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+
+
+// const fruits = ["Mango", "Apple", "Banana", "Grapes", "Kiwi"]
+
+// for(let i = 0; i < fruits.length; i++){
+//   if(fruits[i] === "Banana"){
+//     console.log("Banana Found !")
+//     break;
+//   }else{
+//     console.log(fruits[i])
+//   }
+
+// }
+
+
+// for(let i = 0; i < 5; i++){
+//     if(i == 3){
+//       continue
+//     }
+
+//     console.log(i)
+
+//     // console.log(i)
+// }
+
+
+//! WHILE LOOP !
+
+// let i = 11;
+
+// while (i <= 10) {
+
+//     console.log(i)
+//     i++;
+// }
+
+
+
+// let i = 0;
+
+// while(true){
+   
+// console.log("SORRY !")
+// }
+
+
+
+
+const fruits = ["Mango", "Apple", "Banana", "Grapes", "Kiwi"]
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+// let i =0;
+
+// while(i < fruits.length){
+    
+//     console.log(fruits[0])
+
+    
+//     i++;
+// }
+
+
+let foundShabanaInPakistanCities = ["Islamabad", "Lahore", "Hyderabad", "Karachi", "Faislabad"] 
+
+let shabanaCity = "Karachi"
+
+// let i = 0;
+
+// while(i < foundShabanaInPakistanCities.length){
+//     if(shabanaCity == foundShabanaInPakistanCities[i]){
+//         console.log("Book return tickets !")
+//         break;
+//     }else{
+//         console.log(foundShabanaInPakistanCities[i])
+//     }
+
+//     i++;
+// }
+
+
+// DO WHILE
+
+// let i = 11;
+
+// do{
+//    console.log(i)
+
+// i++
+// }while(i <= 10)
+
+
+
+function abc(){
+    console.log("CALL Chippa Sahab !")
+}
+
+// callSomeOne() 
+// callSomeOne() 
+// callSomeOne() 
+
+
+callSomeOne()
+callSomeOne()
+
+
+console.log('CODE !!!!!!!!!!!!!!!!!!!!!!!!')
+
+
+callSomeOne()
+
+
+console.log('CODE !!!!!!!!!!!!!!!!!!!!!!!!')
+
+callSomeOne()
+
+console.log('CODE !!!!!!!!!!!!!!!!!!!!!!!!')
+callSomeOne()
+callSomeOne()
---
+++ module-2/Javascript/class-12/app.js
@@ -0,0 +1,73 @@
+// FUNCTIONS 
+
+
+// for(let i = 1; i <= 100; i++){
+//     greeting()
+// }
+
+
+
+
+// function greeting(){
+//   return "ALI AFTAB !"
+// }
+
+// console.log(greeting())
+
+// let value;
+
+// function add(){
+//    return 2 + 2
+// }
+// let addValue = add()
+
+// console.log(add())
+// console.log(add())
+// console.log(add())
+// console.log(add())
+
+// console.log(add())
+// console.log(add())
+// console.log(add())
+// console.log(add())
+// console.log(add())
+// console.log(add())
+
+// let addValue = add()
+// console.log(addValue)
+
+
+// console.log(value)
+
+// console.log(abc)
+// const abc = "AHBC"
+
+
+
+
+// function greeting(name){
+//     console.log("HELLO " + name)
+// }
+
+// greeting("Ali")
+// greeting("Waqar")
+// greeting("Hammad")
+// greeting("Okasha")
+
+
+                 //! PARAMETER
+// function add(num1, num2){
+//   return num1 + num2
+// }
+
+// console.log(add(2))
+                //! ARGUEMENT
+
+
+// function subtract(num1 , num2){
+//     return num1 - num2
+// }
+
+// console.log(subtract(10 , 5))
+
+
