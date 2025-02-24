45bcca77a081295c673d1f5faf316ebcf3508a1b
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-02-24 14:51:42 +0500

    class-13

---
+++ module-2/Javascript/class-13/app.js
@@ -0,0 +1,134 @@
+// FUNCTION 
+
+
+// function getMyName(){
+//     console.log("Ali Aftab")
+// }
+
+// getMyName()
+// getMyName()
+// getMyName()
+
+
+
+// function greeting(name){
+//     console.log(`Hello ${name}`)
+// }
+
+// greeting("Ali")
+// greeting("Waqar")
+
+
+// function add(num1, num2){
+
+//     console.log("INSIDE THE FUNCTION", num1 + num2)
+//     return num1 + num2
+// }
+
+
+// console.log(add(3, 3)
+// );
+
+// console.log(add(6, 6)
+// );
+
+
+
+
+// function grading(marks){
+//     if(marks >= 90){
+//         return "A+"
+//     }else if(marks >= 80){
+//         return "A"
+//     }else if(marks >= 70){
+//         return "B"
+//     }else if(marks >= 60){
+//         return "C"
+//     }else if(marks >= 50){
+//         return "D"
+//     }else{
+//         return "Fail"
+//     }
+// }
+
+
+// function addMarks(){
+//     let marks = +prompt("Enter your Percentage", 80)
+//     let grade = grading(marks) 
+
+//     alert("Your Grade is: " + grade)
+// }
+
+// addMarks()
+
+
+// const firstName = "Ali"
+// const lastName = "Aftab"
+
+// function getFullName(firstName, lastName){
+//     const fullName = firstName + " " + lastName
+//     return fullName
+// }
+
+// console.log(getFullName("aLi", "aFtAb"))
+
+
+
+
+// const cities = ["karachi", "lahore", "islamabad", "quetta", "peshawar"]
+
+// const firstName = prompt("Enter your City", "karachi")
+
+
+
+// HOMEWORK: Fix this code issue
+
+// function checkCity(city) {
+//     for (let i = 0; i < cities.length; i++) {
+//         if (city.toLowerCase() === cities[i]) {
+//             alert("City Found")
+//             break
+//         }else{
+//             alert("City Not Found")
+//         }
+//     }
+// }
+
+// checkCity(firstName)
+
+
+
+//ANONYMOUS FUNCTION
+// function(){
+//     return "Ali Aftab"
+// }
+
+// console.log(name())
+
+
+// ARROW FUNCTION
+
+
+// const fullName = function (firstName, lastName){
+//     return `${firstName} ${lastName}`
+// }
+
+// const fullName = (firstName , lastName)=>{
+//     return `${firstName} ${lastName}`
+// }
+
+// const fullName = (firstName, lastName) => `${firstName} ${lastName}`
+
+
+
+// console.log(fullName("Ali", "Aftab"))
+
+
+
+
+// console.log(fname)
+
+// var a = "ABC"
+
+// let fname = "Ali"   //TEMPORAL DEAD ZONE
+// const lname = "Aftab"
---
+++ module-2/Javascript/class-14/app.js
@@ -0,0 +1,29 @@
+// const fullName = "Ali Aftab";
+
+// const modifiedName = fullName.slice(4)
+// console.log(modifiedName)
+
+// console.log(fullName)
+
+
+
+
+// const message = "Yesterday Pakistan won the match!, Pakistan Zindabad"
+// console.log(message.indexOf("Pakistan"))
+// console.log(message.lastIndexOf("Pakistan"))
+
+
+// const fullName = "ali"
+// console.log(fullName.charAt(0).toUpperCase())
+
+
+const text = "Ali"
+
+for (var i = 0; i < text.length; i++) {
+    if (text.charAt(i) === "!") {
+        console.log("Exclamation point found!");
+        break;
+    }else{
+        console.log("Exclamation point not found!");
+    }
+}
