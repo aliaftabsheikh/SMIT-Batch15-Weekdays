759302baabe13c99c9e8c38457ff83622afadcc8
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-02-26 14:11:35 +0500

    class-14

--- module-2/Javascript/class-14/app.js
+++ module-2/Javascript/class-14/app.js
@@ -17,13 +17,75 @@
 // console.log(fullName.charAt(0).toUpperCase())
 
 
-const text = "Ali"
+// HOMEWORK: Fix this code issue
 
-for (var i = 0; i < text.length; i++) {
-    if (text.charAt(i) === "!") {
-        console.log("Exclamation point found!");
-        break;
-    }else{
-        console.log("Exclamation point not found!");
-    }
-}
+// function checkCity(city) {
+//     for (let i = 0; i < cities.length; i++) {
+//         if (city.toLowerCase() === cities[i]) {
+//             alert("City Found")
+//             break
+//         }else{
+//             alert("City Not Found")
+//         }
+//     }
+// }
+
+// checkCity(firstName)
+
+
+//! 2nd PROBLEM
+
+
+// const text = "Ali"
+
+// for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//         console.log("Exclamation point found!");
+//         break;
+//     }else{
+//         console.log("Exclamation point not found!");
+//     }
+// }
+
+
+
+// const message = "Yesterday Pakistan won the match!, Pakistan Shabash"
+
+// const modifiedMessage = message.replace(/Pakistan/g, "India");
+// const modifiedMessage = message.replaceAll("Pakistan", "India");
+
+// console.log(modifiedMessage)
+
+
+
+// const round = 9.99999999999
+// console.log("CEIL",Math.ceil(round))  
+// console.log("FLOOR",Math.floor(round))
+
+
+// const random = Math.floor(Math.random() * 10) + 1
+
+
+// for(let i = 0; i < 30; i++){
+//     console.log(random)
+// }
+
+
+
+// const number = "9.99999abc999999"
+
+
+// console.log("ParseInt",parseInt(number))
+// console.log("ParseFloat",parseFloat(number))
+
+
+// console.log(+number)
+// console.log(Number(number))
+
+// console.log("ToFixed",number.toFixed(2))
+
+
+
+// const random = 1010
+
+// console.log(random.toString().slice(0, 2))
