b1c3c3e60be18bdc9680b7a08a59a113ae1ff5ef
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-03-03 15:29:13 +0500

    class-17

---
+++ module-2/Javascript/class-17/app.js
@@ -0,0 +1,106 @@
+//! FOR LOOP
+
+
+// Initialization
+// Condition
+// Updation
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+//! WHILE LOOP
+
+// Initialization
+// Condition
+// Updation
+
+
+// let i = 1;
+
+// while(i <= 10){
+//     console.log(i)
+
+//     i++;
+// }
+
+
+//! Do While Loop
+
+// Initialization
+// Condition
+// Updation
+
+
+// let i = 11
+
+// do{
+//     console.log(i)
+//     i++;
+// }while(i <= 10)
+
+
+
+// FOR EACH
+// FOR IN
+// FOR OF
+
+
+
+
+// FUNCTION 
+
+
+// function declare/definition
+
+// function greeting(){
+//     console.log("Hello!")
+// }
+
+
+// function calling/invoking
+// greeting()
+
+
+// ARROW FUNCTION 
+
+// const person = {
+//     name: "Ali",
+//     age: 20,
+//     greet : ()=>{
+//         console.log("Hello!")
+//     }
+    
+// }
+
+
+// person.greet()
+
+
+
+//! ARROW FUNCTIONS
+
+
+// const greeting = (name)=> {
+//     if(name === "Kashif"){
+//         return `Imposter name is ${name} !`
+//     }
+
+//     return `Hello ${name}`
+// }
+
+// console.log(greeting("Ali"))
+// console.log(greeting("Qasim"))
+// console.log(greeting("Kashif"))
+
+
+
+function greet(){
+    console.log("Hello, hope you are doing well!")
+}
+
+
+function by(){
+    console.log("By, hope you are experience is good!")
+}
