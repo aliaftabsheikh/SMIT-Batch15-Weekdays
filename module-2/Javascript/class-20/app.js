5c3c0dc106713a790c8b0015200ef2c883087bb3
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-03-12 16:52:31 +0500

    class-20

---
+++ module-2/Javascript/class-20/app.js
@@ -0,0 +1,111 @@
+// const box = document.getElementsByClassName('box')[0]
+
+
+// function increaseBoxSize(){
+    
+//     box.style.width = '500px'
+//     box.style.height = '500px'
+//     box.style.backgroundColor = 'blue'
+//     box.style.fontSize = '60px'
+//     box.style.color = 'white'
+
+// }
+
+// function orignalSize(){
+    
+//     box.style.width = '400px'
+//     box.style.height = '400px'
+//     box.style.backgroundColor = 'pink'
+//     box.style.fontSize = '40px'
+//     box.style.color = 'black'
+
+// }
+
+
+
+
+// alert("Hello World !!!")
+
+
+// var 
+// let 
+// const
+
+// Global variable
+
+// var a = 10;
+
+// // {
+// // }
+
+// a = 20
+// console.log(a)
+
+
+// console.log(a)
+
+
+
+// {
+//     let b = 20;
+//     console.log(b)
+// }
+
+
+
+// let b1 = 30;
+// console.log(b1)
+
+// {
+//     const c = "abc";
+// }
+
+// c = "def"
+
+// console.log(c);
+
+
+
+// let x = 2;
+
+// x++;
+// ++x
+
+// console.log(++x + ++x);
+
+// let x = 9;
+
+// x--;
+// --x
+
+// console.log(--x + --x);
+
+
+
+// for loop
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i , "Hello Shabana !")
+// }
+
+
+const fruits = ["apple", "mango", "banana"]
+
+
+for(let i = 0; i < fruits.length; i++){
+    if(fruits[i] == "mango"){
+        console.log("Mango find !")
+
+        break;
+    }
+
+}
+
+// for(let i = 1; i <= 5; i++){
+//     if(i == 3){
+//         break
+//     }
+
+
+//     console.log(i)
+// }
