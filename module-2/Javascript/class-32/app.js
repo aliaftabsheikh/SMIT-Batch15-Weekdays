43a39fa3ad63c1e86ec447770798c80d89274ef2
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-04-24 02:12:51 +0500

    class-32

---
+++ module-2/Javascript/class-32/app.js
@@ -0,0 +1,20 @@
+// console.log(this.alert("Hello World!"));
+// var a = 10
+// console.log(window);
+
+// console.log(a);
+
+//     var a = 10;
+
+
+a() 
+
+function a (){
+    b()
+    var num = 10
+
+    function b (){
+        console.log(num)
+    }
+}
+
