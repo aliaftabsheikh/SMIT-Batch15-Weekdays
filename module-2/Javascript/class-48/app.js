6cebdeb34a77bef4ae85e188e23b795c56c3184e
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-06-13 15:25:25 +0500

    class-48

---
+++ module-2/Javascript/class-48/app.js
@@ -0,0 +1,14 @@
+class Person {
+    name = "Ali";
+    age = 21
+
+
+    
+}
+
+const p1 = new Person()
+const p2 = new Person()
+const p3 = new Person()
+console.log(p1);
+console.log(p2);
+console.log(p3);
---
+++ module-2/Javascript/class-48/readme.md
@@ -0,0 +1,3 @@
+# OOP SLIDES LINK :
+
+https://drive.google.com/file/d/1TK7E9vgKL9tOhg3oNrDD-SsPWQLo4v1F/view 
