5a4e9e8201dc4c3aa4ef18cdf66bdeb1b9e7ac2b
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-01-19 15:55:38 +0500

    'class-23'

---
+++ module-3/class-23/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+const { productRouter } = require('./routes/products');
+const { authRouter } = require('./routes/auth');
+
+
+app.use('/products', productRouter);
+app.use('/auth', authRouter);
+
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    console.log('Database connected successfully');
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+
+}).catch((err) => {
+    console.log('Database connection failed:', err);
+});
